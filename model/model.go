4eab7682be76c64e2166deaacde897ce100c670d
Zopper9000 <david.lu@hotmail.com>
2016-04-23 20:00:09 -0400

    when will the boilerplate stop
    vanilla js or a fancy framework?

--- model/model.go
+++ model/model.go
@@ -8,6 +8,17 @@ import (
 //Main handler for requests
 func Handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "You have reached %s\n", r.URL.Path[1:]);
+	//Find gameID in database
+		//Send game via JSON if found
+	//Otherwise create new game
+		//Send new game via JSON
+	//Deal with POST requests for moves
+}
+
+//Process board
+func processBoard() {
+	//Processes the current game state
+	//Takes out pieces as needed
 }
 
 func ServerStart() {
