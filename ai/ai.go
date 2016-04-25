9bebb759e468b5f7156bf344a2e9a71c0279122b
Zopper9000 <david.lu@hotmail.com>
2016-04-25 13:05:24 -0400

    wrote a few lines

--- ai/ai.go
+++ ai/ai.go
@@ -1,10 +1,13 @@
 package ai
 
+var board[][] int;
+var g[][] int;
+
 //NextMove based on board state, whether the player is a 1 or a -1
 //Returns x, y, GG
 // GG = true, ai surrenders
 //Currently does not look-ahead moves
-func NextMove(board [][] int, player int) (int, int, bool){
+func NextMove(b [][] int, player int) (int, int, bool){
 	//Find groups (assumes dead groups have been removed)
 	//Classify each group as alive, dead, or unsettled
 	//Find number of liberties for each group
@@ -18,7 +21,7 @@ func NextMove(board [][] int, player int) (int, int, bool){
 
 	//Find groups
 	//ASSUMES SQUARE BOARD
-	var g = [len(board)][len(board)] int;
+	board = b;
 	for i:=0; i < len(board); i++ {
 		for j:=0; j < len(board); j++ {
 			g[i][j] = -1;
@@ -30,9 +33,29 @@ func NextMove(board [][] int, player int) (int, int, bool){
 	for i:=0; i < len(board); i++ {
 		for j:=0; j < len(board); j++ {
 			if(g[i][j] == -1 && board[i][j] != 0) {
-				//TODO
-				//FILL OUT
+				findGroup(i, j, board[i][j], cur);
+				cur++;
 			}
 		}
 	}
 }
+
+//DFS time
+func findGroup(x int, y int, player int, cur int) {
+	if(board[i][j] == player) {
+		g[i][j] = cur;
+
+		if(j-1 >= 0 && board[i][j-1] == player) {
+			findGroup(i, j-1, player, cur);
+		}
+		if(j+1 < len(board) && board[i][j+1] == player) {
+			findGroup(i, j+1, player, cur);
+		}
+		if(i-1 >= 0 && board[i-1][j] == player) {
+			findGroup(i-1, j, player, cur);
+		}
+		if(i+1 < len(board) && board[i+1][j] == player) {
+			findGroup(i+1, j, player, cur);
+		}
+	}
+} 
