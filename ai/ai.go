ded7e7a5f2a66082fd4911d189c64f8768a0af9e
Zopper9000 <david.lu@hotmail.com>
2016-04-24 21:15:20 -0400

    weee

--- ai/ai.go
+++ ai/ai.go
@@ -1,10 +1,10 @@
 package ai
 
-//NextMove based on board state, whether the player is a 1 or a 0
+//NextMove based on board state, whether the player is a 1 or a -1
 //Returns x, y, GG
 // GG = true, ai surrenders
 //Currently does not look-ahead moves
-func NextMove(board [] int, player int) (int, int, bool){
+func NextMove(board [][] int, player int) (int, int, bool){
 	//Find groups (assumes dead groups have been removed)
 	//Classify each group as alive, dead, or unsettled
 	//Find number of liberties for each group
@@ -13,4 +13,26 @@ func NextMove(board [] int, player int) (int, int, bool){
 	//2. Kill easy opponent groups
 	//3. Start new groups
 	//4. Kill difficult opponent groups
+	//TODO: DFS each move up to X moves?
+	//This can be really hard
+
+	//Find groups
+	//ASSUMES SQUARE BOARD
+	var g = [len(board)][len(board)] int;
+	for i:=0; i < len(board); i++ {
+		for j:=0; j < len(board); j++ {
+			g[i][j] = -1;
+		}
+	}
+
+	//BFS through current group
+	var cur = 0;
+	for i:=0; i < len(board); i++ {
+		for j:=0; j < len(board); j++ {
+			if(g[i][j] == -1 && board[i][j] != 0) {
+				//TODO
+				//FILL OUT
+			}
+		}
+	}
 }
