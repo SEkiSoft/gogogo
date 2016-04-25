37dd550fdf5cd3b1540806d586195a6f80c893d0
Zopper9000 <david.lu@hotmail.com>
2016-04-24 20:16:22 -0400

    wrote AI comments
    still reading up go rules

--- ai/ai.go
+++ ai/ai.go
@@ -1,8 +1,16 @@
 package ai
 
-//NextMove based on board state
+//NextMove based on board state, whether the player is a 1 or a 0
 //Returns x, y, GG
 // GG = true, ai surrenders
-func NextMove(board [] int) (int, int, bool){
-
+//Currently does not look-ahead moves
+func NextMove(board [] int, player int) (int, int, bool){
+	//Find groups (assumes dead groups have been removed)
+	//Classify each group as alive, dead, or unsettled
+	//Find number of liberties for each group
+	//In this order:
+	//1. Defend own groups first
+	//2. Kill easy opponent groups
+	//3. Start new groups
+	//4. Kill difficult opponent groups
 }
