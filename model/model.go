5b4a0ba5d787c115e78bfd63957e3d6e47d9f1a2
Zopper9000 <david.lu@hotmail.com>
2016-04-29 15:33:19 -0400

    wrote some handler code

--- model/model.go
+++ model/model.go
@@ -8,24 +8,40 @@ import (
 	"github.com/mattn/sql-lite3"
 )
 
+type Board struct {
+	numLines int;
+	board [][]int;
+}
+
 //Handler to create or load game
 func gameHandler(w http.ResponseWriter, r *http.Request) {
 	//Find gameID in database
-		//Send game via JSON if found
-	//Otherwise create new game
-		//Send new game via JSON
+	id := r.URL.Path[len("/game/"):];
+	b := loadGame(id);
+	//Send game via JSON
 }
 
 //Handler for moves
 func moveHandler(w http.ResponseWriter, r *http.Request) {
 	//Handle moves as needed
+	player := r.URL.Path[len("/move/"):len("/move/")+2];
+	x := r.URL.Path[len("/move/")+2:len("/move/")+4];
+	y := r.URL.Path[len("/move/")+4:len("/move/")+6];
+	//Process move
 	//Refresh view
+}
+
+//Load game
+func loadGame(id string) Board{
+	//Gets gameID, and loads game
 }
 
 func ServerStart() {
 	http.HandleFunc("/game/", gameHandler);
-	http.HandleFunc("/game/move/", moveHandler);
-	http.ListenAndServe(":8080", nil);
+	http.HandleFunc("/move/", moveHandler);
+	http.ListenAndServe(":80", nil);
+	//Disabling HTTPS for now
+	//http.ListenAndServe(":443", nil);
 }
 
 //TODO:
