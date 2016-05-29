687eb8a48a355b6aeeb2320049766839be071113
David Lu <david.lu@hotmail.com>
2016-05-29 16:34:22 -0400

    Added license

---
+++ store/sql_move_store.go
@@ -0,0 +1,2 @@
+// Copyright (c) 2016 David Lu
+// See License.txt
--- store/store.go
+++ store/store.go
@@ -4,9 +4,7 @@
 package store
 
 import (
-	l4g "github.com/alecthomas/log4go"
 	"github.com/davidlu1997/gogogo/model"
-	"time"
 )
 
 type StoreResult struct {
