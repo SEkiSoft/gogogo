427a55485dde67a00bb52890de309f9c78c491e9
David Lu <david.lu@hotmail.com>
2016-06-01 20:33:49 -0400

    Added server and version

--- gogogo.go
+++ gogogo.go
@@ -1,4 +1,41 @@
 // Copyright (c) 2016 David Lu
 // See License.txt
 
-package gogogo
+package main
+
+import (
+	"github.com/davidlu1997/gogogo/api"
+	"github.com/davidlu1997/gogogo/model"
+	"github.com/davidlu1997/gogogo/store"
+	"github.com/davidlu1997/gogogo/utils"
+)
+
+func doLoadConfig(filename string) (err string) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = r.(string)
+		}
+	}()
+	utils.LoadConfig(filename)
+	return ""
+}
+
+func main() {
+	if err := doLoadConfig(flagConfigFile); err != "" {
+		l4g.Exit("Unable to load configuration file: %s", errstr)
+		return
+	}
+
+	l4g.Info("GoGoGo version %s", model.CurrentVersion)
+
+	api.NewServer()
+	api.InitApi()
+
+	api.StartServer()
+
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGINT, syscallSIGTERM)
+	<-class
+
+	api.StopServer()
+}
---
+++ model/version.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2016 David Lu
+// See License.txt
+
+package model
+
+var versions = []string{
+	"0.0.0",
+}
+
+var CurrentVersion string = version[0]
