a00c66671d064a1cbe6df0a2158043466ebd5f4e
DavidLu1997 <david.lu@hotmail.com>
2016-08-08 23:37:41 -0400

    added player api

--- api/player.go
+++ api/player.go
@@ -4,19 +4,16 @@
 package api
 
 import (
-	l4g "github.com/alecthomas/log4go"
-	"github.com/davidlu1997/gogogo/model"
 	"net/http"
 	"strings"
+
+	"github.com/davidlu1997/gogogo/model"
 )
 
 func InitPlayer() {
 	BaseRoutes.Players.Handle("/create", ApiHandler(createPlayer)).Methods("POST")
 
 	BaseRoutes.NeedPlayer.Handle("/update", ApiPlayerRequired(updatePlayer)).Methods("POST")
-	BaseRoutes.NeedPlayer.Handle("/newpassword", ApiPlayerRequired(updatePassword)).Methods("POST")
-	BaseRoutes.NeedPlayer.Handle("/newusername", ApiPlayerRequired(updateUsername)).Methods("POST")
-	BaseRoutes.NeedPlayer.Handle("/newemail", ApiPlayerRequired(updateEmail)).Methods("POST")
 	BaseRoutes.NeedPlayer.Handle("/get", ApiPlayerRequired(getPlayer)).Methods("GET")
 	BaseRoutes.NeedPlayer.Handle("/games", ApiPlayerRequired(getPlayerGames)).Methods("GET")
 	BaseRoutes.NeedPlayer.Handle("/find", ApiPlayerRequired(findPlayer)).Methods("POST")
@@ -64,18 +61,6 @@ func updatePlayer(s *Session, w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(updatedPlayer.ToJson()))
 }
 
-func updatePassword(s *Session, w http.ResponseWriter, r *http.Request) {
-
-}
-
-func updateUsername(s *Session, w http.ResponseWriter, r *http.Request) {
-
-}
-
-func updateEmail(s *Session, w http.ResponseWriter, r *http.Request) {
-
-}
-
 func getPlayer(s *Session, w http.ResponseWriter, r *http.Request) {
 
 }
@@ -90,7 +75,6 @@ func findPlayer(s *Session, w http.ResponseWriter, r *http.Request) {
 
 func CreatePlayer(player *model.Player) (*model.Player, *model.Error) {
 	if result := <-Srv.Store.Player().Save(player); result.Err != nil {
-		l4g.Error("Create player save error: %s", result.Err)
 		return nil, result.Err
 	} else {
 		registeredPlayer := result.Data.(*model.Player)
@@ -101,11 +85,14 @@ func CreatePlayer(player *model.Player) (*model.Player, *model.Error) {
 
 func UpdatePlayer(player *model.Player) (*model.Player, *model.Error) {
 	if result := <-Srv.Store.Player().Update(player); result.Err != nil {
-		l4g.Error("Player update error: %s", result.Err)
 		return nil, result.Err
 	} else {
 		updatedPlayer := result.Data.(*model.Player)
 
 		return updatedPlayer, nil
 	}
+}
+
+func GetPlayer(id string) (*model.Player, *model.Error) {
+
 }
